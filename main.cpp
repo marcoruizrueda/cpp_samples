2d98b3add9928dfe011e69e023007192b608d482
Marco A <marcoruizrueda@gmail.com>
2023-01-21 19:01:09 +0000

    "Arrays"

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ int main()
     std::cout << "Number 2: " << n2 <<std::endl;
     std::cout << "Number 3: " << n3 <<std::endl;
     std::cout << "Number 4: " << n4 <<std::endl;
-*/
+
     // Braced Initializers
     int val {}; //Initializes to 0
     int val1 {10}; //Initializes to 10
@@ -23,7 +23,6 @@ int main()
     int sum {val + val1};
 
     std::cout << "Sum: " << sum <<std::endl;
-
     
    // Functional Initialization
    int apple_count(5);
@@ -31,17 +30,53 @@ int main()
    int fruits (apple_count + orange_count);
    std::cout << "fruits: " << fruits <<std::endl;
     
-
     // Assignment notation
     int bike = 2;
     int truck = 7;
     int vehicles = bike + truck;
     std::cout << "vehicles: " << vehicles << std::endl;
     std::cout << "sizeof int: " << sizeof(int) << std::endl;
-    std::cout << "sizeof variable: " << sizeof(val1) << std::endl;
+    std::cout << "sizeof variable: " << sizeof(vehicles) << std::endl;
 
     // since x is [[maybe_unused]], no warning generated
     [[maybe_unused]] int xx { 5 };
-    return 0;
 
+    // Fill vector values
+    int scores[10]; // Declare Array of 10 integers
+    for( size_t i{0}; i<10; ++i ){
+        scores[i] = i;
+    }
+
+    // Display values
+    for( size_t i{0}; i<std::size(scores); ++i ){
+        std::cout << "i=" << i << " : " << scores[i] << std::endl;
+    }
+
+    int r {0};
+    for( auto value : scores ){
+        std::cout << scores[r] <<std::endl;
+        r++;
+    }
+
+    // Easier
+    for( auto value : scores ){
+        std::cout << value <<std::endl;
+    }
+
+
+    // Constant arrays can't be modified
+    const int multipliers [] {22,30,15};
+
+    // Omit size of array at declaration
+    int class_sizes [] {10, 20, 30, 12};
+    */
+    // If a charcacter array is null terminated, it's called as C-String
+    char message1 [] {'H', 'i', '\0'};
+    std::cout << message1 << std::endl;
+    std::cout << "Size of message1: " << sizeof(message1) << std::endl;
+
+
+
+
+    return 0;
 }
