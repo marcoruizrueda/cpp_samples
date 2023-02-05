da1299a7d1d846b19daba2c218b994ac49609e0c
Marco A <marcoruizrueda@gmail.com>
2023-02-05 18:26:02 +0000

    makeword

--- misc/main.cpp
+++ misc/main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 
+#define HIBYTE(word)             (static_cast<uint8>((static_cast<uint16>(word))>>8))
+#define LOBYTE(word)             (static_cast<uint8>((static_cast<uint16>(word))&0x00FF))
+#define MAKEWORD(hibyte,lobyte)  (((static_cast<uint16>(hibyte))<<8) | ((static_cast<uint16>(lobyte))&0x00FF))
+
+
 int main()
 {
     /*
@@ -23,13 +28,13 @@ int main()
     int sum {val + val1};
 
     std::cout << "Sum: " << sum <<std::endl;
-    
+
    // Functional Initialization
    int apple_count(5);
    int orange_count(15);
    int fruits (apple_count + orange_count);
    std::cout << "fruits: " << fruits <<std::endl;
-    
+
     // Assignment notation
     int bike = 2;
     int truck = 7;
@@ -69,13 +74,14 @@ int main()
 
     // Omit size of array at declaration
     int class_sizes [] {10, 20, 30, 12};
-    */
+    
     // If a character array is null terminated, it's called as C-String
-    char message1 [] {'H', 'i', '\0'};
+    char message1[]{'H', 'i', '\0'};
     std::cout << message1 << std::endl;
     std::cout << "Size of message1: " << sizeof(message1) << std::endl;
-
-
+    */
+    
+    // ---------------------------
 
 
     return 0;
