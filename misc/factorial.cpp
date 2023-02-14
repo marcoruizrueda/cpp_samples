76948006b16aabc1452d95e8bf7017c307067b3e
Marco A <marcoruizrueda@gmail.com>
2023-02-14 17:39:20 +0000

    Create factorial.cpp

---
+++ misc/factorial.cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+#include <iostream>
+
+int calculateFactorial(int n)
+{
+    if(n==1)
+        return 1;
+    return n * calculateFactorial(n-1);
+}
+
+int main() {
+    int n;
+    std::cin >> n;
+    std::cout << calculateFactorial(n) << std::endl;
+    
+    return 0;
+}
