fb2ae62002afee638cbf06bef49105c3caaf41a0
Marco A <marcoruizrueda@gmail.com>
2023-02-16 15:54:00 +0000

    Update fibo.cpp

--- misc/fibo.cpp
+++ misc/fibo.cpp
@@ -3,34 +3,64 @@
 
 int fiboRecursive(int n)
 {
-    if(n <= 1)
-        return n;
-    return fiboRecursive(n - 1) + fiboRecursive(n - 2);
+    int f = 0;
+    if (n == 1)
+    {
+        f = 0;
+    }
+    else if (n==2)
+    {
+        f = 1;
+    }
+    else
+    {
+        f = fiboRecursive(n - 1) + fiboRecursive(n - 2);
+    }
+    std::cout << n << ": " << f << "\n";
+    return f;
 }
 
-int fibo(int n, bool print)
+int fibo(int n)
 {
     int firstElement = 0; 
     int secondElement = 1;
-    
+    int i = n;
     int f = firstElement + secondElement;
-    
-    if(print)
-        std::cout << firstElement << std::endl << secondElement << std::endl;
-    
-    for(int i=2; i < n; ++i)
+
+    for(int i=1; i <=n; ++i)
     {
-        if(print)
-            std::cout << f << std::endl;
+        if (i == 1)
+        {
+            f = 0;
+        }
+        else if (i==2)
+        {
+            f = 1;
+        }
+        else
+        {
+            firstElement = secondElement;
+            secondElement = f;
+            f = firstElement + secondElement;
+        }
         
-        firstElement = secondElement;
-        secondElement = f;
-        f = firstElement + secondElement;
+        std::cout << i << ": " << f << std::endl;
     }
+    
     return f;
 }
 
 int main(){
-    std::cout << fiboRecursive(9) << std::endl;
-    std::cout << fibo(9, false) << std::endl;
+    int n;
+    std::cout << "Input N number of elements for Fibonacci: ";
+    std::cin >> n;
+    if (n > 0){
+        int f = fibo(n);
+        //std::cout << n << ": " << f << std::endl;
+    }else{
+        std::cout << "Error, n must be > 0";
+    }
+    
+    return 0;
+    //
 }
